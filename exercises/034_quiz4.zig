13e137dc4e6d2aa70b31748c1ecb22e55ee1fdbf
Brandon Cisneros <brcisneros1991@gmail.com>
2024-02-26 16:55:21 -0500

    from office

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -14,12 +14,11 @@ pub fn main() void {
 
     const my_num: u32 = getNumber();
 
-    try stdout.print("my_num={}\n", .{my_num});
+    stdout.print("my_num={}\n", .{my_num});
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
 fn getNumber() NumError!u32 {
-
     if (false) return NumError.IllegalNumber;
     return 42;
 }
